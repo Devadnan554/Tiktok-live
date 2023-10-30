d125eeb651e3ff4687bb81d498d9f2784e7a5eac
Adnan alharbi <140106528+Devadnan554@users.noreply.github.com>
2023-10-30 13:24:35 -0700

    Add files via upload

--- Tiktok.py
+++ Tiktok.py
@@ -36,33 +36,45 @@ print ('''
                  السناب : devadnan                                                                                                                        
 ##### ##### ##### ##### ##### ##### ##### ##### ##### #####  
 ''')  
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
 username = input(green_color+"UserName For Client--> "+detect_color)
 client: TikTokLiveClient = TikTokLiveClient(unique_id="@"+username)
 @client.on("connect")
 async def on_connect(_: ConnectEvent):
+    now = datetime.now()
+    current_time = now.strftime("%I:%M:%S")
     print(Fore.BLUE+"["+current_time+"] "+ green_color+"["+" Connect For id room ] ", client.room_id)
 async def on_comment(event: CommentEvent): 
+    now = datetime.now()
+    current_time = now.strftime("%I:%M:%S")
     print(Fore.LIGHTBLUE_EX+"["+current_time+"] "+ Fore.LIGHTYELLOW_EX+ "[ Comment from ] "+ green_color+f"{event.user.nickname} -->"+Fore.BLUE +f" {event.comment}")
 client.add_listener("comment", on_comment)
 @client.on("disconnect")
 async def on_disconnect(event: DisconnectEvent):
+    now = datetime.now()
+    current_time = now.strftime("%I:%M:%S")
     print(detect_color+"Disconnected")
 @client.on("like")
 async def on_like(event: LikeEvent):
+    now = datetime.now()
+    current_time = now.strftime("%I:%M:%S")
     print(Fore.LIGHTBLUE_EX+"["+current_time+"] "+ Fore.LIGHTYELLOW_EX+ "[ Like from ] "+ green_color+f"{event.user.nickname}")
 @client.on("live_end")
 async def on_connect(event: LiveEndEvent):
     print(f"Ended Live :(")
 @client.on("join")
 async def on_join(event: JoinEvent):
+    now = datetime.now()
+    current_time = now.strftime("%I:%M:%S")
     print(Fore.LIGHTBLUE_EX+"["+current_time+"] "+ Fore.LIGHTYELLOW_EX+ "[ join ] "+ green_color+f"{event.user.nickname}")
 @client.on("share")
 async def on_share(event: ShareEvent):
+    now = datetime.now()
+    current_time = now.strftime("%I:%M:%S")
     print(Fore.LIGHTBLUE_EX+"["+current_time+"] "+ Fore.LIGHTYELLOW_EX+ "[ Shareing ] "+ green_color+f"{event.user.nickname}")
 @client.on("follow")
 async def on_follow(event: FollowEvent):
+     now = datetime.now()
+     current_time = now.strftime("%I:%M:%S")
      print(Fore.LIGHTBLUE_EX+"["+current_time+"] "+ Fore.LIGHTYELLOW_EX+ "[ Add ] "+ green_color+f"{event.user.nickname}")
 if __name__ == '__main__':
     client.run()
